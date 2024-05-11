d9655ccaa9420bbe578bc2e6138fcf7c4044f2ef
Salah_Saad <91617795+salahsaads@users.noreply.github.com>
2024-05-11 12:37:18 +0300

    Home repo return type

--- bookly/lib/Features/Home/data/repos/Home_repo.dart
+++ bookly/lib/Features/Home/data/repos/Home_repo.dart
@@ -1,8 +1,13 @@
 
+import 'package:bookly/Features/Home/data/model/book_model/book_model.dart';
+import 'package:bookly/core/errors/errors.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo{
 
 
-  fetchBeastSellerBooks();
- fetchFeaturedBooks();
-
+   Future<Either<Failure, List<BookModel>>> fetchNewsetBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category});
 }
